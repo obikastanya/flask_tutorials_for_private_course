cd11a18f4b9a1af7adb509dc03e5576139a796c6
Obi Kastanya <obi.kastanya@sigma-tech.co.id>
2023-04-26 13:57:06 +0700

    login with session

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import (
     render_template,
     request,
     redirect,
-    make_response
+    session
 )
 
 from app.home.home_controller import HomeController
@@ -20,21 +20,22 @@ service_controller = ServiceController()
 login_controller = LoginController()
 
 app = Flask(__name__)
+app.secret_key = "supersecretkeysupersecretkey"
 
 @app.get("/login")
 def login():
-    if request.cookies.get("user"):
+    if session.get("user"):
         return redirect("/")
     return render_template("login.html")
 
 
 @app.get("/logout")
 def logout():
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
-    response = make_response ( redirect("/login") )
-    response.delete_cookie("user")
-    return response
+    session.pop("user")
+    # session.clear()
+    return redirect("/login")
     
 
 @app.post("/login")
@@ -49,20 +50,18 @@ def login_post():
     user = result[2]
 
     if login_status:
-        response = make_response( redirect("/") )
-        user_email = user.get("email")
-        response.set_cookie("user", user_email)
-        return response
+        session["user"] = user.get('email')
+        return redirect("/") 
     
     return render_template("login.html", error_message=error_message)
 
 
 @app.get("/")
 def home():
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
     data = home_controller.get_home_page_data()
-    user = request.cookies.get("user")
+    user = session.get("user")
     return render_template(
         "index.html", 
         clients = data.get("clients"), 
@@ -74,47 +73,47 @@ def home():
 
 @app.get("/product")
 def product():
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
     products = product_controller.get_products()
-    user = request.cookies.get("user")
+    user = session.get("user")
     return render_template("product.html", products = products, user = user)
 
 
 @app.get("/product/<product_id>/")
 def product_detail(product_id):
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
     product = product_controller.get_product_by_id(product_id)
-    user = request.cookies.get("user")
+    user = session.get("user")
     return render_template("product_detail.html", product = product, user = user)
 
 @app.get("/award")
 def award():
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
     awards = award_controller.get_awards()
-    user = request.cookies.get("user")
+    user = session.get("user")
     return render_template("award.html", awards = awards, user = user)
 
 @app.get("/service")
 def service_get():
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
     service_kinds = service_controller.service_kind.get_service_kinds()
-    user = request.cookies.get("user")
+    user = session.get("user")
     return render_template("service.html", service_kinds = service_kinds, user = user)
 
 @app.post("/service")
 def service_post():
-    if not request.cookies.get("user"):
+    if not session.get("user"):
         return redirect("/login")
     service_kind_id = request.form.get("service_kind")
     amount = request.form.get("amount")
     insurance = request.form.get("insurance")
     installation_support = request.form.get("installation_support")
     shipping = request.form.get("shipping")
-    user = request.cookies.get("user")
+    user = session.get("user")
 
     price_estimation = service_controller.calculate_price_estimation(
         service_kind_id,
